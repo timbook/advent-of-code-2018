3a3345af69c213286def2bf07448bcf3cfa6de73
Tim Book <timothykbook@gmail.com>
2018-12-01 08:54:10 -0500

    day 1 part 2

--- day-01.py
+++ day-01.py
@@ -1,6 +1,21 @@
+import sys
+
 with open("data/01-input.txt", 'r') as f:
     lines = f.readlines()
 
 items = [int(n.strip()) for n in lines]
 
-print(sum(items))
+print(f"Part 1 Answer: {sum(items)}")
+
+record = set()
+freq = 0
+done = False
+while True:
+    for i in items:
+        freq += i
+        if freq in record:
+            print(f"Part 2 Answer: {freq}")
+            sys.exit(0)
+        else:
+            record.add(freq)
+
